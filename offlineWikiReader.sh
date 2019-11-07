0ffaa53af15cd953992c347cb3e5e310adabc6bc
Michael Hoereth <no-reply@pentabyte.de>
2019-11-07 22:17:20 +0100

    text style added to console output

--- offlineWikiReader.sh
+++ offlineWikiReader.sh
@@ -1,6 +1,11 @@
 clear
+
+BOLD="\x1B[1m"
+PLAIN="\x1B[0m"
+INVERTED="\x1B[7m"
+
 echo "####################################################"
-echo "# Welcome to a very simple offline Wiki Reader!    #"
+echo "# Welcome to a very simple ${BOLD}offline Wiki Reader${PLAIN}!    #"
 echo "#                              by Michael Hoereth  #"
 echo "# Requirements:                                    #"
 echo "#   * compressed wiki pages multistream download   #"
@@ -21,11 +26,13 @@ then
    echo "[lang]wiki-latest-pages-articles-multistream.xml.bz2 (compressed)"
    echo "You can get such files from https://dumps.wikimedia.org"
    exit;
+else
+   echo "WIKI_DOWNLOADS environment is set to: ${BOLD}${WIKI_DOWNLOADS}${PLAIN}"
 fi
 read -p "Which Wiki? [en]? " LANG
 LANG=${LANG:-en}
 read -p "Page title (regular expression, :title$ for exact match)? " PATTERN
-echo "Search results: (Byte Offset : Page ID : Page Title)"
+echo "Search results: (${INVERTED}Byte Offset${PLAIN} : ${INVERTED}Page ID${PLAIN} : ${INVERTED}Page Title${PLAIN})"
 grep "${PATTERN}" -i ${WIKI_DOWNLOADS}/${LANG}wiki-latest-pages-articles-multistream-index.txt
 read -p "Byte Offset? " OFFSET
 read -p "Page ID? " ARTICLEID
@@ -38,10 +45,10 @@ dd skip=${OFFSET} count=1000000 if=${WIKI_DOWNLOADS}/${LANG}wiki-latest-pages-ar
 && echo "<pages>" | cat - temp.xml > pages.xml \
 && rm temp.xml \
 && echo "</pages>" >> pages.xml \
-&& xmllint --xpath "//id[text()='${ARTICLEID}']/.." pages.xml --recover > page.xml && echo "page.xml with full page content has been extracted." \
+&& xmllint --xpath "//id[text()='${ARTICLEID}']/.." pages.xml --recover > page.xml && echo "${BOLD}page.xml${PLAIN} with full page content has been extracted." \
 && rm pages.xml \
 && TITLE=$(xmllint --xpath "//title/text()" page.xml) \
-&& echo "Page Title: ${TITLE}" \
-&& xmllint --xpath "//text/text()" "page.xml" > text.md && echo "text.md with page text has been extracted." \
-&& pandoc text.md -o text.html && echo "text.html has been created from text.md." \
+&& echo "Page Title: ${INVERTED}${TITLE}${PLAIN}" \
+&& xmllint --xpath "//text/text()" "page.xml" > text.md && echo "${BOLD}text.md${PLAIN} with page text has been extracted." \
+&& pandoc text.md -o text.html && echo "${BOLD}text.html${PLAIN} has been created from text.md." \
 && echo "You can now open your preferred file with the app of your choice."
